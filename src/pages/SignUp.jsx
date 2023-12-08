db23dfdce8833954387fccd77e73d9453dbbe175
jsgiovani <=>
2023-12-07 21:23:53 -0500

    UI register user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,32 +1,66 @@
-import React from 'react'
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
+import axiosConnection from '../config/axios';
 
 const SignUp = () => {
+
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+
+    //handle form submit fn
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        
+        const data = {
+            username: usernameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+
+        try {
+            const request = await axiosConnection.post('/api/auth/register',{...data});
+
+            console.log(request.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
+
+
   return (
     <div className='mx-auto md:max-w-lg'>
         <h1 className='text-3xl font-bold text-slate-800 text-center my-7'>Sign Up</h1>
 
-        <form className='px-2 space-y-4'>
+        <form className='px-2 space-y-4' onSubmit={(e) => {handleSubmit(e)}}>
 
             <input 
                 type="text" 
                 placeholder='Username'
-                className='bg-white w-full p-2 border rounded-md'  
+                className='bg-white w-full p-2 border rounded-md'
+                ref={usernameRef}  
             />
 
             <input 
                 type="email" 
                 placeholder='Email'
-                className='bg-white w-full p-2 border rounded-md'  
+                className='bg-white w-full p-2 border rounded-md'
+                ref={emailRef}  
             />
 
             <input 
                 type="password" 
                 placeholder='Password'
-                className='bg-white w-full p-2 border rounded-md'  
+                className='bg-white w-full p-2 border rounded-md'
+                ref={passwordRef}  
             />
 
-            <button className='w-full bg-slate-700 text-white uppercase p-2 rounded-lg hover:opacity-95'>
+            <button 
+                className='w-full bg-slate-700 text-white uppercase p-2 rounded-lg hover:opacity-95'
+            >
                 Sign up
             </button>
 
@@ -36,7 +70,7 @@ const SignUp = () => {
 
         </form>
 
-        <div className='p-2 flex gap-2'>
+        <div className='p-2 gap-2 hidden'>
             <p>Have an account?</p>
             <Link className='text-blue-700' to="/sign-in">Sign in</Link>
         </div>
