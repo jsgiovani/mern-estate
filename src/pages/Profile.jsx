06f18b8cfe42a3b795d49efa916953266f6d3ff3
jsgiovani <=>
2023-12-11 15:28:11 -0500

    update/change user photo profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,60 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
 
     const {currentUser} = useSelector((state) => state.user);
+    const fileRef = useRef(null);
     const [alerts, setAlerts] = useState([]);
+    const [file, setFile ] = useState(undefined);
+    const [uploadPorcentage, setUploadPorcentage] = useState();
+    const [formData, setFormData] = useState({});
+
+  
+
+    //fn to upload image to server
+    const uploadImage = (file)=>{
+
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storeRef = ref(storage, fileName);
+        const uploadFile = uploadBytesResumable(storeRef, file);
+
+        uploadFile.on('state_changed', 
+        
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setUploadPorcentage(`Upload is ${parseInt(progress)}% done`);
+            },
+
+            (error) =>  {
+                setAlerts('Image must be max: 2MB');
+                setUploadPorcentage(null);
+            },
+
+            ()=>{
+                getDownloadURL(uploadFile.snapshot.ref).then((downloadURL) =>{
+                    setFormData({...formData, photo: downloadURL});
+                    setAlerts(null);
+                    console.log(formData);
+                })
+            }
+        )
+
+    }
+
+
+    useEffect(() => {
+        if (file) {
+            uploadImage(file)
+        }
+    }, [file])
+
+    
+
+
 
 
     //handle form submit fn
@@ -19,17 +69,28 @@ const Profile = () => {
 
 
         {alerts && (
-            <p className='px-2 text-red-500 rounded-md mb-4'>{alerts}</p>
+            <p className='px-2 text-red-500 rounded-md mb-4 text-center'>{alerts}</p>
         )}
 
         <form className='px-2 space-y-5' onSubmit={(e) => {handleSubmit(e)}}>
 
-            <div className="flex justify-center items-center">
+            <input 
+                className="hidden" 
+                type="file" 
+                ref={fileRef}
+                accept="image/png, image/jpeg"
+                onChange={(e) => {setFile(e.target.files[0])}}  
+            />
+
+            <div className="flex justify-center items-center flex-col">
                 <img 
-                    className="rounded-full  w-24 h-24 object-cover"
-                    src={currentUser.user.photo} 
+                    className="rounded-full  w-24 h-24 object-cover cursor-pointer"
+                    src={formData.photo || currentUser.user.photo} 
                     alt="user img"
+                    onClick={()=>fileRef.current.click()}
                 />
+
+                <small className="text-center pt-2 text-green-700">{uploadPorcentage}</small>
             </div>
 
             <input 
