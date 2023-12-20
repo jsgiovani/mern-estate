d7a958a1b44065cd8bee1afb22bbdde1fe3b2fd3
jsgiovani <=>
2023-12-20 16:44:14 -0500

    add contact owner functionality

---
+++ src/components/Contact.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+import axiosConnection from '../config/axios';
+
+const Contact = ({property}) => {
+
+
+    const [owner, setOwner] = useState({});
+
+
+
+    const fetchOwner = async(ownerId)=>{
+        try {
+            const {data} = await axiosConnection.get(`/api/users/${ownerId}`, {
+                headers:{
+                    'authorization': localStorage.getItem('token'),
+                }
+            });
+
+            setOwner({...data});
+            
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }
+
+
+    useEffect(() => {
+    
+    }, [fetchOwner(property.userRef)])
+
+
+    
+
+  return (
+    <>
+        {owner && owner.username && (
+            <div className='my-6'>
+                <p className='mb-2'>Contact <span className='font-semibold'>{owner.username}</span> for <span className='font-semibold'>{property.name}</span> </p>
+                <textarea 
+                    id="message" 
+
+                    rows="5"
+                    className='bg-white w-full p-2 border rounded-md resize-none'
+                    placeholder='Message'
+                    onChange={(e) =>{}}
+                ></textarea>
+
+                <button className='bg-green-800 py-2 px-8 text-white font-semibold rounded-lg text-sm mt-4 w-full md:w-auto'>
+                    Send
+                </button>
+
+            </div>
+
+        )}
+
+    </>
+  )
+}
+
+export default Contact
--- src/pages/property/Property.jsx
+++ src/pages/property/Property.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axiosConnection from '../../config/axios';
+import { FaBath, FaBed } from 'react-icons/fa';
+import { FaSquareParking } from "react-icons/fa6";
+import { BiFridge } from "react-icons/bi";
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { useSelector } from 'react-redux';
+import Contact from '../../components/Contact';
 
 
 const Property = () => {
+
+    const {currentUser} = useSelector((state) => state.user);
+
     const {id} = useParams();
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false)
-    const [property, setProperty] = useState({})
+    const [property, setProperty] = useState({});
+    const [contact, setContact] = useState(false);
 
 
     //swiper
@@ -55,10 +64,10 @@ const Property = () => {
         );
     }
 
-    console.log(property);
 
   return (
     <main>
+
         {property.imageUrls && !isLoading && !error && (
             <Swiper
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
@@ -72,20 +81,76 @@ const Property = () => {
                             style={{ 
                                 background:`url(${url})`,
                                 backgroundRepeat:'no-repeat',
-                                // backgroundSize:'cover',
+                                backgroundSize:'cover',
                                 backgroundPosition:'center center' 
                             }}
                         >
-                           
+
                         </div>
                     </SwiperSlide>
                 ))}
-               
+
             </Swiper> 
 
         )}
 
-         
+
+        <div className='px-2 md:max-w-6xl md:mx-auto'>
+            <h1 className='font-bold text-3xl my-10'>{property.name} - ${property.offer ? property.discountPrice : property.regularPrice} {property.type ==='rent' ? '/month': ''} </h1>
+            <div className='flex gap-2 items-center my-10'>
+                <span className='bg-red-800 py-1 px-5 text-white font-semibold rounded-lg text-sm'>{property.type ==='rent' ? 'For Rent': 'For Sale'}</span>
+                {property.offer && (
+                    <span className='bg-green-800 py-1 px-5 text-white font-semibold rounded-lg text-sm'>${property.regularPrice - property.discountPrice} discount</span>
+                )}
+            </div>
+
+            <p className='mt-5 text-slate-800'>{property.description}</p>
+
+            <ul className='my-5 flex items-center gap-4 flex-wrap'>
+                <li className='text-green-900 flex gap-1 items-center font-bold text-xl'>
+                    <FaBed />
+                    <small>{property.bedrooms} Beds</small>
+                </li>
+
+                <li className='text-green-900 flex gap-1 items-center font-bold text-xl'>
+                    <FaBath/>
+                    <small>{property.bathrooms} Beds</small>
+                </li>
+
+                <li className='text-green-900 flex gap-1 items-center font-bold text-xl'>
+                    <FaSquareParking />
+
+                    {property.parking ? (
+                        <small>Parking</small>
+                    ):(
+                        <small>No parking</small>
+                    )}
+                </li>
+
+                <li className='text-green-900 flex gap-1 items-center font-bold text-xl'>
+                    <BiFridge  />
+
+                    {property.parking ? (
+                        <small>Furnished</small>
+                    ):(
+                        <small>No Furnished</small>
+                    )}
+                </li>
+            </ul>
+
+            {currentUser && currentUser.user._id!==property.userRef && !contact && (
+                <button onClick={()=>{setContact(true)}} className='uppercase bg-slate-700 text-white rounded-lg px-6 py-2 font-bold w-full md:w-auto my-5'>
+                    Contact owner
+                </button>
+
+            )}
+
+
+            {contact && (
+                <Contact property={property}/>
+            )}
+
+        </div>
     </main>
   )
 }
